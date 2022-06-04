b9b8bb10d91b66beb7c78c13cc4d3a05f09656e0
Welde-Ab <meneliksahalu@gmail.com>
2022-06-04 22:39:09 +0900

    print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if(len(my_list)==0):
-        return None;
+    if(len(my_list) == 0):
+        return None
     else:
         for i in range(len(my_list)):
-            print("{}".format(my_list[i]));
-            
+            print("{}".format(my_list[i]))
