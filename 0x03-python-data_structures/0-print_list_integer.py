e04b8fcca5a62628f4c68027899c3dab511ef05f
Welde-Ab <meneliksahalu@gmail.com>
2022-06-04 22:28:42 +0900

    print_list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    if(len(my_list)==0):
+        return;
+    else:
+        for i in range(len(my_list)):
+            "{}".format(my_list[i]);
+        
