15192339f53fed9123b819aabd2e09c6a2a39622
Welde-Ab <meneliksahalu@gmail.com>
2022-06-22 23:03:16 +0900

    Further define square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This program is about squares"""
+
+
+class Square:
+    """This class defines fields and methods of squares"""
+    def __init(self, size):
+        """This method accepts the initial value of the square"""
+        self.__size = size
